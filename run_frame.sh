8cf9dc1e497cee202a9a4cadd417cec5ae36d0b2
zhs <zouhaosheng@163.com>
2017-05-08 14:11:12 +0800

    LSTM with audio, --batch_size 128 --num_epochs 7 --clip_gradient_norm 4

--- run_frame.sh
+++ run_frame.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
-model=DbofModel
-CUDA_VISIBLE_DEVICES=2 python train.py --model=$model --train_data_pattern="/mfs/shiyu/yt8m/train/train*" --train_dir=./Results/$model --frame_features=True --feature_names="rgb" --batch_size 128 # --base_learning_rate 0.001 # --num_epochs 500 --export_model_steps 7000 --clip_gradient_norm 8
+model=LstmModel
+CUDA_VISIBLE_DEVICES=0 python train.py --model=$model --train_data_pattern="/mfs/shiyu/yt8m/train/train*" --train_dir=./Results/$model --frame_features=True --feature_names="rgb, audio" --feature_sizes="1024, 128" --batch_size 128  --base_learning_rate 0.001 --num_epochs 7 --export_model_steps 20000 --clip_gradient_norm 4
